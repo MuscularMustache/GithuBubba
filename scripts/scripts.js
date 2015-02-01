6de5d5426bc0f89951184b9055f097a4c84a0224
gabriel parke <gabe1029384756@yahoo.com>
2015-02-01 18:30:13 -0500

    Toggle + signs from diff's
    When removing + signs, it also removes line comment btn. I need to fix
    that

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,36 @@
-var fileTitle = $('.js-selectable-text');
+if (typeof String.prototype.startsWith != 'function') {
+  String.prototype.startsWith = function (str){
+    return this.indexOf(str) == 0;
+  };
+}
 
-fileTitle.on('click', function(){
+$('.actions .show-file-notes').after('<button class="minibutton collapsable">Hide Diff</button>');
+$('.actions .show-file-notes').after('<button class="minibutton hide-add-content">Toggle +</button>');
+
+$('body').on('click', '.minibutton.collapsable', function(){
   var $self = $(this);
   var diffContent = $self.parents('.js-details-container').find('.highlight.blob-wrapper');
-  if ($self.hasClass('closed')) {
-    $self.removeClass('closed');
-    diffContent.css('display', 'block');
+  if (diffContent.hasClass('closed')) {
+    diffContent.removeClass('closed');
+    $self.text('Hide Diff');
   } else {
-    $self.addClass('closed');
-    diffContent.css('display', 'none');
-  }  
+    diffContent.addClass('closed');
+    $self.text('Show Diff');
+  }
+});
+
+$('body').on('click', '.minibutton.hide-add-content', function(){
+  var button = $(this);
+  var addContent = button.parents('.js-details-container').find('.highlight.blob-wrapper .blob-code.blob-code-addition');
+
+  addContent.each(function(){
+    var $self = $(this); // each code addition line
+    var origText = $self.text();
+    if (origText.startsWith('+')) {
+      $self.text($self.text().substring(1));
+    } else {
+      $self.text('+' + origText);
+    }
+  });
+
 });
